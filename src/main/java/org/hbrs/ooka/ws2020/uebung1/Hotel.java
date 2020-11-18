b7839f189cb10ba039e2021965fa2545b963dbe4
Hasan <pai.7ce@gmail.com>
2020-11-18 13:08:04 +0100

    revert

--- src/main/java/org/hbrs/ooka/ws2020/uebung1/Hotel.java
+++ src/main/java/org/hbrs/ooka/ws2020/uebung1/Hotel.java
@@ -3,7 +3,6 @@ package org.hbrs.ooka.ws2020.uebung1;
 public class Hotel {
     private String name;
     private String location;
-    private boolean f = true;
 
     public Hotel(String name, String location) {
         this.name = name;
